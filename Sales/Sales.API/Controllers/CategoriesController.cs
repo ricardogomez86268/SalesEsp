c6957b4800e73d894294d6a08b8a1b98657f7290
ricardogomez86268 <ricardogomez86268@correo.itm.edu.co>
2023-05-01 12:39:02 -0500

    Paginacion y filtro de categoria

--- Sales/Sales.API/Controllers/CategoriesController.cs
+++ Sales/Sales.API/Controllers/CategoriesController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Sales.API.Data;
+using Sales.API.Helpers;
 using Sales.Shared.Entities;
+using Sales.Shared.DTOs;
 
 namespace Sales.API.Controllers
 {
@@ -18,10 +19,35 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAsync()
+        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
         {
-            return Ok(await _context.Categories
+            var querable = _context.Categories
+                         .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                querable = querable.Where(c => c.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            return Ok(await querable
+                .OrderBy(c => c.Name)
+                .Paginate(pagination)
                 .ToListAsync());
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
+        {
+            var querable = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                querable = querable.Where(c => c.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
+            double count = await querable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
         }
 
         [HttpGet("{id}")]
